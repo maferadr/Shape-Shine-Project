6d9e866b3ed29785cd704dd4019b64a55246fd6e
maferadr <maferadr0604@gmail.com>
2024-07-15 17:47:11 -0400

    Workout page updated

--- src/pages/Workouts.jsx
+++ src/pages/Workouts.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import Video from "../components/Video";
 
 export default function Workouts(){
 
@@ -48,13 +47,6 @@ export default function Workouts(){
                     </div>
                 ))}
             </div>
-
-            <div className="triangle black-triangle hidden">
-                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
-                </svg>
-            </div>
-            {/* <Video/> */}
         </div>
     )
 };
